6fa8b43aa4a1b50c48e8bb56f2578c7e5d0b6544
sid77320 <cancel.cyril@yahoo.fr>
2015-12-01 16:02:38 +0100

    initial commitpuisque le fichier index.php a eté crée avant depuis brackets

---
+++ index.php
@@ -0,0 +1,19 @@
+<!doctype html>
+
+<html lang="fr">
+    <head>
+        <meta charset="utf-8"/>
+        <title>
+            Base page HTML5
+        </title>
+
+        <!-- police google -->
+
+        <link rel="stylesheet"href="css/normalize.css"/>
+        <link rel="stylesheet"href="css/stylejs1.css"/>
+    </head>
+
+    <body>
+      
+    </body>
+</html>
